970581ff02dc61922193a95363c7cb0866c0a592
kodashim <kodashim@yahoo-corp.jp>
2020-10-25 21:04:18 +0900

    update

---
+++ work/js/modal.js
@@ -0,0 +1,8 @@
+// モーダルを開く
+function modal_open(target) {
+ document.getElementById(target).style.display = "block";
+}
+// モーダルをボタンで閉じる
+function modal_close(target) {
+ document.getElementById(target).style.display = "none";
+}
